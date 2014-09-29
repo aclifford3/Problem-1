e1c7deabb3cfcd6376f2ef3a1473aebbeb68fa1c
aclifford3 <alec.clifford@gmail.com>
2014-09-29 13:12:49 -0400

    Update Problem1.m

--- Problem1.m
+++ Problem1.m
@@ -18,7 +18,7 @@
     %Initial conditions
     V_m = [zeros(1, timeLen)];
     V_i = 0;    
-    %Equations
+    
     a_m = 0.1*((25-V_i)/(exp((25-V_i)/10)-1));
     b_m = 4*exp(-V_i/18);
     a_n = 0.01*((10-V_i)/(exp((10-V_i)/10)-1));
@@ -26,7 +26,7 @@
     a_h = 0.07*exp(-V_i/20);
     b_h = 1/(exp((30-V_i)/10)+1);
     
-    %Initial conditions
+    
     m = a_m / (a_m + b_m);
     n = a_n / (a_n + b_n);
     h = a_h / (a_h + b_h);
@@ -46,41 +46,47 @@
         a_h = 0.07*exp(-V_m(i-1)/20);
         b_h = 1/(exp((30-V_m(i-1))/10)+1);
         
-        %Update currents
-        
+        %Calculate differentials of gating probabilities
         dm = a_m * (1-m) - b_m * m;
         dn = a_n * (1-n) - b_n * n;
         dh = a_h * (1-h) - b_h * h;
-        %Update channel probabilities
+       
+        %Update channel probabilities using Euler's method
         m = m + stepSize*dm;
         n = n + stepSize*dn;
         h = h + stepSize*dh;
         
+        %Calculate currents
         I_Na = m^3*gbar_Na*h*(V_m(i-1) - E_Na);
         I_K = n^4 * gbar_K * (V_m(i-1) - E_K);
         I_L = gbar_L * (V_m(i-1) - E_L);
         I_ion = I_in - I_K - I_Na - I_L;
         
+        %Update ion channel conductances
         g_Na(i) = m.^3*gbar_Na*h;
         g_K(i) = n^4*gbar_K;
         g_L(i) = gbar_L;
         
-        
+        %Calculate membrance potential change
         dV_m = I_ion / C_m;
   
         
-        %Update membrance voltage
+        %Update membrane potential
          V_m(i) = V_m(i-1) + stepSize * dV_m;
         
         
     end
+    %Shift entire system by -70 mV to enter correct biological value range
     V_m = V_m + V_rest;
+    %Graphs Voltage vs. Time
     figure();
     plot(time, V_m);
     axis([0,100, -100, 100]);
     title('Voltage vs. Time');
     xlabel('Time (ms)');
     ylabel('Membrane voltage (mV)');
+    
+    %Graphs Ion conductance vs. time
     figure();
     plot(time, g_Na, time, g_K);
     xlabel('Time (ms)');
